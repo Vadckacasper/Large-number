6579ae7770c91d38b4498d5898c9043d7de3f3fb
Vadka.casper@gmail.com <Vadka.casper@gmail.com>
2020-10-01 22:22:36 +0700

    Добавьте файлы проекта.

---
+++ Multiplication/BigNum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargeNumber
+{
+    class BigInt
+    {
+        private byte[] Whole;
+        private int SizeWhole;
+
+        public static implicit operator BigInt(string number)
+        {
+            BigInt Designer = new BigInt();
+            Designer.Conversion(number);
+            return Designer;
+        }
+
+        private void Conversion(string number)
+        {
+            Whole = new byte[number.Length];
+            foreach (char point in number)
+            {
+                Whole[SizeWhole] += byte.Parse(Convert.ToString(point)); //плохо временно
+                SizeWhole++;
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < SizeWhole; i++)
+            {
+                Console.Write(Whole[i]);
+            }
+        }
+    }
+}
---
+++ Multiplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LargeNumber;
+namespace Multiplication
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInt big;
+            big = "123456789";
+            big.Print();
+            Console.ReadKey();
+        }
+    }
+}
